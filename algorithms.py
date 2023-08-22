a0dccfbb6f7582baca37eee439ca059043c5ee71
Manoa987 <enzo.ruedas@outlook.com>
2023-08-22 14:55:58 -0300

    Main completed

--- algorithms.py
+++ algorithms.py
@@ -2,6 +2,7 @@ import copy
 import time
 import sys
 
+
 class SokobanSolver:
     class TreeNode:
         def __init__(self, state, parent=None, action=None, depth=0):  # Add depth parameter with default value
@@ -16,18 +17,22 @@ class SokobanSolver:
             new_row = []
             for i, elem in enumerate(row):
                 if elem == state.Icons.WALL or (
-                        (state.get_cell_content(i,j-1)==state.Icons.WALL and state.get_cell_content(i-1,j)==state.Icons.WALL) or
-                        (state.get_cell_content(i,j-1)==state.Icons.WALL and state.get_cell_content(i+1,j)==state.Icons.WALL) or
-                        (state.get_cell_content(i,j+1)==state.Icons.WALL and state.get_cell_content(i-1,j)==state.Icons.WALL) or
-                        (state.get_cell_content(i,j+1)==state.Icons.WALL and state.get_cell_content(i+1,j)==state.Icons.WALL)
-                    ):
-                        new_row.append(sys.maxsize)
+                        (state.get_cell_content(i, j - 1) == state.Icons.WALL and state.get_cell_content(i - 1,
+                                                                                                         j) == state.Icons.WALL) or
+                        (state.get_cell_content(i, j - 1) == state.Icons.WALL and state.get_cell_content(i + 1,
+                                                                                                         j) == state.Icons.WALL) or
+                        (state.get_cell_content(i, j + 1) == state.Icons.WALL and state.get_cell_content(i - 1,
+                                                                                                         j) == state.Icons.WALL) or
+                        (state.get_cell_content(i, j + 1) == state.Icons.WALL and state.get_cell_content(i + 1,
+                                                                                                         j) == state.Icons.WALL)
+                ):
+                    new_row.append(sys.maxsize)
                 else:
-                    value = min([self._manhattan_distance((i,j), goal) for goal in state.get_goals()])
+                    value = min([self._manhattan_distance((i, j), goal) for goal in state.get_goals()])
                     new_row.append(value)
             matrix.append(new_row)
         return matrix
-    
+
     def __init__(self, initial_sokoban):
         self.initial_node = self.TreeNode(initial_sokoban)
         self.grid = self._grid_notation(self.initial_node.state)
@@ -41,13 +46,13 @@ class SokobanSolver:
             node_counter += 1
 
             if self._goal_test(node.state):
-                return node, node_counter
+                return node, node_counter, len(frontier), node.depth
 
             for action in self._actions_fn(node.state):
                 new_state = self._apply_action(node.state, action)
-                frontier.append(self.TreeNode(new_state, node, action))
+                frontier.append(self.TreeNode(new_state, node, action, node.depth + 1))
 
-        return None, node_counter
+        return None, node_counter, len(frontier), 0
 
     def _dfs(self):
         frontier = [self.initial_node]
@@ -58,28 +63,29 @@ class SokobanSolver:
             node_counter += 1  # Increment the counter for each explored node
 
             if self._goal_test(node.state):
-                return node, node_counter
+                return node, node_counter, len(frontier), node.depth
 
             for action in self._actions_fn(node.state):
                 new_state = self._apply_action(node.state, action)
-                frontier.append(self.TreeNode(new_state, node, action))
-        return None , node_counter# If goal not found
+                frontier.append(self.TreeNode(new_state, node, action, node.depth + 1))
+        return None, node_counter, len(frontier), 0  # If goal not found
 
     def _dls(self, node, depth, max_depth, node_counter):
         if depth > max_depth:
-            return None, node_counter
+            return None, node_counter, node.depth
         if self._goal_test(node.state):
-            return node, node_counter
+            return node, node_counter, node.depth
         if depth == 0:
-            return 'cutoff', node_counter
+            return 'cutoff', node_counter, node.depth
         for action in self._actions_fn(node.state):
             new_state = self._apply_action(node.state, action)
-            child, node_counter = self._dls(self.TreeNode(new_state, node, action), depth - 1, max_depth, node_counter)
+            child, node_counter = self._dls(self.TreeNode(new_state, node, action, node.depth + 1), depth - 1,
+                                            max_depth, node_counter)
             if child == 'cutoff':
-                return 'cutoff', node_counter
+                return 'cutoff', node_counter, node.depth
             elif child is not None:
-                return child, node_counter
-        return None, node_counter
+                return child, node_counter, node.depth
+        return None, node_counter, node.depth
 
     def _iddfs(self, max_depth):
         node_counter = 0
@@ -89,9 +95,56 @@ class SokobanSolver:
                 return result, node_counter
         return None, node_counter
 
+    def _local_greedy(self, node_counter, heuristic_fn):
+        def h(node):
+            return heuristic_fn(node.state)
+
+        def recursive_local_greedy(node, node_counter):
+            if self._goal_test(node.state):
+                return node, node_counter, node.depth
+
+            frontier = []
+            for action in self._actions_fn(node.state):
+                new_state = self._apply_action(node.state, action)
+                new_node = self.TreeNode(new_state, node, action, node.depth + 1)
+                frontier.append(new_node)
+
+            while frontier:
+                node = min(frontier, key=h)
+                frontier.remove(node)
+                child, node_counter = recursive_local_greedy(node, node_counter)
+                if child is not None:
+                    return child, node_counter, node.depth
+            return None, node_counter, node.depth
+
+        return recursive_local_greedy(self.initial_node, node_counter)
+
+    def _global_greedy(self, heuristic_fn):
+        def h(node):
+            return heuristic_fn(node.state)
+
+        frontier = [self.initial_node]
+        node_counter = 0
+
+        while frontier:
+            node = min(frontier, key=h)
+            frontier.remove(node)
+            node_counter += 1
+
+            if self._goal_test(node.state):
+                return node, node_counter, len(frontier), node.depth
+
+            for action in self._actions_fn(node.state):
+                new_state = self._apply_action(node.state, action)
+                new_node = self.TreeNode(new_state, node, action, node.depth + 1)
+                frontier.append(new_node)
+
+        return None, node_counter, len(frontier), 0
+
     def _a_star(self, heuristic_fn):
         def f(node):
             return node.depth + heuristic_fn(node.state)
+
         frontier = [self.initial_node]
         node_counter = 0
 
@@ -101,37 +154,15 @@ class SokobanSolver:
             node_counter += 1
 
             if self._goal_test(node.state):
-                return node, node_counter
+                return node, node_counter, len(frontier), node.depth
 
             for action in self._actions_fn(node.state):
                 new_state = self._apply_action(node.state, action)
-                new_node = self.TreeNode(new_state, node, action)
+                new_node = self.TreeNode(new_state, node, action, node.depth + 1)
                 new_node.depth = node.depth + 1
                 frontier.append(new_node)
 
-        return None, node_counter
-    
-    def _greedy(self, heuristic_func):
-        def f(node):
-            return heuristic_func(node.state)
-
-        frontier = [self.initial_node]
-        node_counter = 0  # Initialize node counter
-
-        while frontier:
-            node = min(frontier, key=f)
-            frontier.remove(node)
-            node_counter += 1  # Increment node counter
-
-            if self._goal_test(node.state):
-                return node, node_counter  # Return the node object and node counter
-
-            for action in self._actions_fn(node.state):
-                new_state = self._apply_action(node.state, action)
-                new_node = self.TreeNode(new_state, node, action)
-                frontier.append(new_node)
-
-        return None, node_counter  # Return None and node counter if no solution is found
+        return None, node_counter, len(frontier), 0
 
     def _goal_test(self, state):
         return state.level_complete()
@@ -155,39 +186,42 @@ class SokobanSolver:
             min_distance = min([self._manhattan_distance(box, goal) for goal in state.get_goals()])
             total_distance += min_distance
         return total_distance
-    
+
     def _heuristic_manhattan_player(self, state):
-        x,y, _ = state.get_player()
-        min_distance = min([self._manhattan_distance((x,y), box) for box in state.get_boxes()])
+        x, y, _ = state.get_player()
+        min_distance = min([self._manhattan_distance((x, y), box) for box in state.get_boxes()])
         return min_distance
-    
+
     def _heuristic_freedom_degrees(self, state):
         nb_freedom_degrees = 0
         for box in state.get_boxes():
             nb_freedom_degrees += 4
-            x,y = box
-            if state.get_cell_content(x+1,y) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
-            if state.get_cell_content(x,y+1) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
-            if state.get_cell_content(x-1,y) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
-            if state.get_cell_content(x,y-1) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
-        return (nb_freedom_degrees/len(state.get_boxes()))/4
-    
+            x, y = box
+            if state.get_cell_content(x + 1, y) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
+            if state.get_cell_content(x, y + 1) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
+            if state.get_cell_content(x - 1, y) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
+            if state.get_cell_content(x, y - 1) not in [state.Icons.BOX, state.Icons.WALL]: nb_freedom_degrees -= 1
+        return (nb_freedom_degrees / len(state.get_boxes())) / 4
+
     def _combined_heuristics(self, state):
         manhattan_boxes = self._heuristic_manhattan_boxes(state)
         manhattan_player = self._heuristic_manhattan_player(state)
         freedom_degrees = self._heuristic_freedom_degrees(state)
         return manhattan_boxes + manhattan_player + freedom_degrees
-    
+
     def _grid_heuristic(self, state):
         val = 0
-        for j,i in state.get_boxes():
+        for j, i in state.get_boxes():
             val += self.grid[i][j]
         return val
-             
+
     def play_sokoban_with_algorithm(self, algorithm_name, heuristic_name, max_depth):
+        heuristic_func = ""
         start_time = time.time()
         result = None
         node_counter = 0
+        node_frontier = 0
+        depth = 0
 
         # Choose the heuristic to use
         if (heuristic_name not in ["manhattan_boxes", "manhattan_player", "freedom_degrees", "combined", "grid"]):
@@ -204,20 +238,20 @@ class SokobanSolver:
             heuristic_func = self._grid_heuristic
 
         # Choose the algorithm to use
-        if (algorithm_name not in ["bfs", "dfs", "iddfs", "greedy", "a_star"]):
+        if (algorithm_name not in ["bfs", "dfs", "iddfs", "local_greedy", "global_greedy", "a_star"]):
             raise ValueError("Invalid algorithm name")
         elif algorithm_name == "bfs":
-            result, node_counter = self._bfs()
+            result, node_counter, node_frontier, depth = self._bfs()
         elif algorithm_name == "dfs":
-            result = self._dfs()
+            result, node_counter, node_frontier, depth = self._dfs()
         elif algorithm_name == "iddfs":
-            result, node_counter = self._iddfs(max_depth)
-        elif algorithm_name == "greedy":
-            heuristic_func = self._combined_heuristics
-            result, node_counter = self._greedy(heuristic_func)
+            result, node_counter, depth = self._iddfs(max_depth)
+        elif algorithm_name == "local_greedy":
+            result, node_counter, depth = self._local_greedy(0, heuristic_func)
+        elif algorithm_name == "global_greedy":
+            result, node_counter, node_frontier, depth = self._global_greedy(heuristic_func)
         elif algorithm_name == "a_star":
-            heuristic_func = self._combined_heuristics
-            result, node_counter = self._a_star(heuristic_func)
+            result, node_counter, node_frontier, depth = self._a_star(heuristic_func)
         print(f"Algorithm : {algorithm_name}")
         print(f"with heuristic : {heuristic_name}")
         end_time = time.time()
@@ -227,6 +261,18 @@ class SokobanSolver:
         if result:
             print("Solution found:")
             print(f"Nodes visited: {node_counter}")
+            print(f"Nodes frontera: {node_frontier}")
+            print(f"Costo: {depth}")
+
+            def rec_path(node):
+                if (node.parent is None):
+                    x, y, _ = node.state.get_player()
+                    print(f"X: {x}, Y: {y}")
+                    return
+                rec_path(node.parent)
+                x, y, _ = node.state.get_player()
+                print(f"X: {x}, Y: {y}")
+            rec_path(result)
         else:
             print("No solution found")
         print(f"In {execution_time}s")
--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from algorithms import SokobanSolver
 from sokoban import Sokoban
 
+
 def main():
-    
     # Create an instance of the Sokoban class with the desired level and levels file
-    level = 55
+    level = 53
     levels_file = "levels.txt"
+    algorithms = ["bfs", "global_greedy", "a_star"]
+    heuristics = ["manhattan_boxes", "manhattan_player", "combined", "grid"]
     initial_sokoban = Sokoban(level, levels_file)
 
     # Specify the algorithm name and maximum depth (for IDDFS)
     algorithm_name = "a_star"  # Choose the algorithm: "bfs", "dfs", "greedy", "a_star", "iddfs"
-    heuristic_name = "combined" # Choose the algorithm: "manhattan_boxes", "manhattan_player", "freedom_degrees", "combined", "grid"
+    heuristic_name = "grid"  # Choose the algorithm: "manhattan_boxes", "manhattan_player", "freedom_degrees", "combined", "grid"
     max_depth = 4000  # Set the maximum depth for IDDFS (ignored for other algorithms)
 
     sokobanSolver = SokobanSolver(initial_sokoban)
+
+    #sokobanSolver.play_sokoban_with_algorithm(algorithm_name, heuristic_name, max_depth)
+
+    for algorithm in algorithms:
+        if algorithm != "bfs":
+            for heuristic in heuristics:
+                sokobanSolver.play_sokoban_with_algorithm(algorithm, heuristic, max_depth)
+        else:
+            sokobanSolver.play_sokoban_with_algorithm(algorithm, heuristic_name, max_depth)
+
     # Use the specified algorithm to play Sokoban
-    sokobanSolver.play_sokoban_with_algorithm(algorithm_name, heuristic_name, max_depth)
-    
+
+
+
 # Main entry point
 if __name__ == "__main__":
     main()
